ec8e21322740d7b09b56f99fd11e3c97d283a312
Vitu <vitorcsharp@hotmail.com>
2024-07-29 22:13:51 -0300

    Criando constantes globais de validacao que se utiliza de propriedades do FluentValidation

---
+++ Src/BuildingBlocks/DesafioDvD.Core/ValidationMessages/ValidationMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace DesafioDvD.Core.ValidationMessages
+{
+    public static class ValidationMessages
+    {
+        //Constantes de validacoes para utilizar nas validacoes dos CQRS
+        public const string MIN_LENGTH_ERROR_MESSAGE = "{PropertyName} must have at least {MinLength} characters";
+        public const string MAX_LENGTH_ERROR_MESSAGE = "{PropertyName} must not reach {MaxLength} characters";
+        public const string EMPTY_STRING_ERROR_MESSAGE = "{PropertyName} can not be empty";
+        public const string ERROR_MESSAGE = "Invalid {PropertyName}";
+    }
+}
+
+//{PropertyName} -> O fluentValidation passa o nome da propriedade que a gente ta setando
+//{MinLength} -> Nome do erro que estamos passando la na validacao
+//{MaxLength}} -> Nome do erro que estamos passando la na validacao

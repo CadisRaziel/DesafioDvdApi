10e6b5a4a2bc8395cf99fe02b646735651e4463e
Vitu <vitorcsharp@hotmail.com>
2024-08-02 16:00:43 -0300

    Criando command para alugar um dvd

---
+++ Src/Services/Publisher/Application/DesafioDvD.Application/Features/Dvds/Commands/RentDvd/RentDvdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace DesafioDvD.Application.Features.Dvds.Commands.RentDvd
+{
+    //Atualizacao do estado do dvd (Alugar um DVD)
+    public record RentDvdCommand(Guid Id) : IRequest<RentDvdResponse>;   
+}
---
+++ Src/Services/Publisher/Application/DesafioDvD.Application/Features/Dvds/Commands/RentDvd/RentDvdResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace DesafioDvD.Application.Features.Dvds.Commands.RentDvd
+{
+    public class RentDvdResponse(string Id, DateTime UpdatedAt);
+}

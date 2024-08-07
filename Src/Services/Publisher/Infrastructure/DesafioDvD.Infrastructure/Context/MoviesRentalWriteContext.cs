0b410272785fb89f17094c73f90f927ad984f463
Vitu <vitorcsharp@hotmail.com>
2024-08-07 16:57:17 -0300

    criando contexto do banco de dados a partir do EF e instanciando os bancos que ele vai criar

---
+++ Src/Services/Publisher/Infrastructure/DesafioDvD.Infrastructure/Context/MoviesRentalWriteContext.cs
@@ -0,0 +1,22 @@
+﻿
+using DesafioDvD.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioDvD.Infrastructure.Context
+{
+    public class MoviesRentalWriteContext : DbContext
+    {
+        public MoviesRentalWriteContext()
+        {
+            
+        }
+
+        public MoviesRentalWriteContext(DbContextOptions<MoviesRentalWriteContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Dvd> Dvds { get; set; }
+    }
+}

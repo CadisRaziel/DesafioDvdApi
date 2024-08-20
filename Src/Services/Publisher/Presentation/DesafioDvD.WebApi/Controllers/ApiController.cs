2f1ddec470d06d2a28a465410b8c8bff0d7e899c
Vitu <vitorcsharp@hotmail.com>
2024-08-19 21:29:18 -0300

    Criando Classe para tratar responses, criando controller do director

---
+++ Src/Services/Publisher/Presentation/DesafioDvD.WebApi/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using DesafioDvD.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioDvD.WebApi.Controllers
+{
+    //ApiVersion -> do pacaote Asp.Versioning.MVC
+    //v{version:apiVersion} -> vai pegar o valor daqui [ApiVersion("1")]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        //Metodo de resposta customizada utilizada no nosso Controller
+        protected ActionResult CustomResponse(int status, bool success, object data = null)
+        {
+            //(status, success) -> Tupla, dois retornos
+            return (status, success) switch
+            {
+                (404, false) => NotFound(new BaseResponse { StatusCode = status, Success = success, Message = "No element found." }),
+                (400, false) => BadRequest(new BaseResponse { StatusCode = status, Success = success, Message = "Erros during the transaction." }),
+                (201, true) => Ok(new BaseResponse { StatusCode = status, Success = success, Message = "Created", Data = data }),
+                (200, true) => Ok(new BaseResponse { StatusCode = status, Success = success, Data = data }),
+            };
+        }
+    }
+}
---
+++ Src/Services/Publisher/Presentation/DesafioDvD.WebApi/Controllers/DirectorController.cs
@@ -0,0 +1,76 @@
+﻿using DesafioDvD.Application.Features.Directors.Commands.CreateDirector;
+using DesafioDvD.Application.Features.Directors.Commands.DeleteDirector;
+using DesafioDvD.Application.Features.Directors.Commands.UpdateDirector;
+using DesafioDvD.Core;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DesafioDvD.WebApi.Controllers
+{
+    public class DirectorController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public DirectorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("[action]/{fullName}", Name = "GetDirector")]
+        [ProducesResponseType(typeof(BaseResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetDirector([FromRoute] string fullName)
+        {
+            var query = new GetDirectorQuery(fullName);
+
+            var response = await _mediator.Send(query, HttpContext.RequestAborted);
+
+            if (response is null)
+                return CustomResponse((int)HttpStatusCode.NotFound, false);
+
+            return CustomResponse((int)HttpStatusCode.OK, true, response);
+        }
+
+        [HttpPost("CreateDirector")]
+        [ProducesResponseType(typeof(CreateDirectorResponse), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<CreateDirectorResponse>> createDirector([FromBody] CreateDirectorCommand command)
+        {
+            var response = await _mediator.Send(command, HttpContext.RequestAborted);
+
+            if(response is null)
+                return CustomResponse((int)HttpStatusCode.BadRequest, false);
+
+            return CustomResponse((int)HttpStatusCode.Created, true, response);
+        }
+
+
+        [HttpPost("UpdateDirector")]
+        [ProducesResponseType(typeof(UpdateDirectorResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<UpdateDirectorResponse>> UpdateDirector([FromBody] UpdateDirectorCommand command)
+        {
+            var response = await _mediator.Send(command, HttpContext.RequestAborted);
+
+            if(response is null)
+                return CustomResponse((int)HttpStatusCode.BadRequest, false);
+
+            return CustomResponse((int)HttpStatusCode.OK, true, response);
+        }
+
+        [HttpPost("DeleteDirector/{id:guid}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> DeleteDirector([FromRoute] Guid id)
+        {
+            var command = new DeleteDirectorCommand(id);
+            var response = await _mediator.Send(command, HttpContext.RequestAborted);
+
+            if(!response)
+                return CustomResponse((int)HttpStatusCode.BadRequest, response);
+
+            return CustomResponse((int)HttpStatusCode.OK, response);
+        }
+    }
+}

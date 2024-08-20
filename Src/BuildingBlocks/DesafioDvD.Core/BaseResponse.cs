78760ce2a33c1bf877f30a40933ecf36691af2b6
Vitu <vitorcsharp@hotmail.com>
2024-08-19 21:29:46 -0300

    Criando Response generica para ser atribuida ao tratamento de erros de controller

---
+++ Src/BuildingBlocks/DesafioDvD.Core/BaseResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DesafioDvD.Core
+{
+    public class BaseResponse
+    {
+        public int StatusCode { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; } //-> aqui sera nossos objetos (DvdResponse etc..)
+    }
+}
